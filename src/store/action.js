ebd1ad7abbc2cffa3cb8aefc33173348a8ecb316
johnson <kryzbone@gmail.com>
2020-09-25 07:24:49 +0000

    firebase cloud storage setup

--- src/store/action.js
+++ src/store/action.js
@@ -1,8 +1,22 @@
 const action = (type, payload = {}) => {
-    return {
-        type,
-        payload
-    }
-}
 
-export default action
+    return (dispatch, state, { getFirestore }) => {
+
+        // Add a new document with a generated id to firestore.
+        getFirestore()
+          .collection("contacts").doc(payload.id.toString())
+          .set(payload)
+          .then(() => {
+            //dispatch to update the state
+            dispatch({type, payload})
+          })
+          .catch(err => console.log(err))
+    } 
+};
+
+export const addContact = (data) => action("addContact", data);
+export const editUser = (data) => action("editUser", data);
+export const deleteUser = (data) => action("deleteUser", data);
+export const addTag = (data) => action("addTag", data);
+
+export default action;
